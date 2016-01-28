56c0db02b722a76e404a8791a38bd8bb8fbae2c1
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2016-01-28 14:20:29 +0000

    dots connected

--- js/gameServices.ts
+++ js/gameServices.ts
@@ -1,9 +1,9 @@
 angular.module('gameServices', [])
 .factory('GameMoves', function(){
 	function whoseMove(grid, colNo, rowNo) {
-		if(grid[colNo][rowNo] == 1) {
+		if(grid[colNo][rowNo] === 1) {
 			return 1;
-		} else if(grid[colNo][rowNo] == 2) {
+		} else if(grid[colNo][rowNo] === 2) {
 			return 2;
 		}
 	}
@@ -11,25 +11,16 @@ angular.module('gameServices', [])
 	function relativeMoves (grid, colNo, rowNo, playerNum) {
 		var _class = [];
 		
-		if(grid[rowNo - 1] && grid[colNo][rowNo - 1] == playerNum) {
-			_class.push("connect-bottom");
-		} else if(grid[rowNo + 1] && grid[colNo][rowNo + 1] == playerNum) {
-			_class.push("connect-top");
-		} else if(grid[colNo - 1] && grid[colNo - 1][rowNo] == playerNum) {
-			_class.push("connect-right");
-		} else if(grid[colNo + 1] && grid[colNo + 1][rowNo] == playerNum) {
-			_class.push("connect-left");
-		} else if(grid[rowNo - 1] && grid[colNo - 1] && grid[colNo - 1][rowNo - 1] == playerNum) {
-			_class.push("connect-bottom-right");
-		} else if(grid[colNo + 1] && grid[rowNo - 1] && grid[colNo + 1][rowNo - 1] == playerNum) {
-			_class.push("connect-bottom-left");
-		} else if(grid[rowNo + 1] && grid[colNo - 1] && grid[colNo - 1][rowNo + 1] == playerNum) {
-			_class.push("connect-top-left");
-		} else if(grid[colNo + 1] && grid[rowNo + 1] && grid[colNo + 1][rowNo + 1] == playerNum) {
-			_class.push("connect-top-right");
-		}
+		if (grid[rowNo - 1] && grid[colNo][rowNo - 1] === playerNum) _class.push("connect-bottom");
+		if (grid[rowNo + 1] && grid[colNo][rowNo + 1] === playerNum) _class.push("connect-top");
+		if (grid[colNo - 1] && grid[colNo - 1][rowNo] === playerNum) _class.push("connect-left");
+		if (grid[colNo + 1] && grid[colNo + 1][rowNo] === playerNum) _class.push("connect-right");
+		if (grid[rowNo - 1] && grid[colNo - 1] && grid[colNo - 1][rowNo - 1] === playerNum) _class.push("connect-bottom-left");
+		if (grid[colNo + 1] && grid[rowNo - 1] && grid[colNo + 1][rowNo - 1] === playerNum) _class.push("connect-bottom-right");
+		if (grid[rowNo + 1] && grid[colNo - 1] && grid[colNo - 1][rowNo + 1] === playerNum) _class.push("connect-top-left");
+		if (grid[colNo + 1] && grid[rowNo + 1] && grid[colNo + 1][rowNo + 1] === playerNum) _class.push("connect-top-right");
 		
-		if(_class.length) {
+		if (_class.length) {
 			_class.push("connected");
 		}
 		
@@ -38,8 +29,8 @@ angular.module('gameServices', [])
 
 	function move(grid, colNo, rowNo) {
 		let _whoseMove = whoseMove(grid, colNo, rowNo),
-		_relativeMove = relativeMoves(grid, colNo, rowNo, _whoseMove),
-		_return = ` player-${_whoseMove}-move ${_relativeMove}`;
+			_relativeMove = relativeMoves(grid, colNo, rowNo, _whoseMove),
+			_return = ` player-${_whoseMove}-move ${_relativeMove}`;
 
 		return _return;
 	}
